c39a6c3fe95f8e9c7fe6d788df6320f45f9694dc
bmys <bmys94@gmail.com>
2019-05-12 00:52:31 +0200

    add main class

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,5 +1,22 @@
+import java.io.File
+import java.io.InputStream
+
 object MainKt {
     @JvmStatic
     fun main(args: Array<String>) {
+        if(args[0] == "t"){
+            val fileName = args[1]
+            val t = startTransmission(File(fileName), System.`in`, System.out)
+        }
+        else{
+            val sign:ControlChars;
+            if(args[2] == "crc"){
+                sign = ControlChars.NAK
+            }
+            else{
+                sign = ControlChars.C
+            }
+            val t = start(sign, System.`in`, System.out)
+        }
     }
 }
